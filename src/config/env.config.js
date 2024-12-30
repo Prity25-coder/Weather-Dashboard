ba6a596ba46ddca7eeb9f4f03c640c4f1eff6623
Prity25-coder <pritynishad480@gmail.com>
2024-12-30 14:20:42 +0530

    change env file

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -1,9 +1,8 @@
-const { VITE_BASE_URL, VITE_DAILY_VAR } = import.meta.env;
-console.log("daily", VITE_DAILY_VAR);
+const { VITE_BASE_URL } = import.meta.env;
 
 const config = {
   baseUrl: String(VITE_BASE_URL),
-  dailyVar: JSON.parse(VITE_DAILY_VAR)
+  dailyVar: ["temperature_2m_max","temperature_2m_min","temperature_2m_mean","apparent_temperature_max","apparent_temperature_min","apparent_temperature_mean"]
 };
 
 export default config;
